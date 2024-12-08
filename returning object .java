5398faedd26b84297a51de88b05b4284652eb97f
roshani <roshanisalekar3046@gmail.com>
2024-12-08 19:48:47 +0530

    Add files via upload

---
+++ returning object .java
@@ -0,0 +1,27 @@
+// object are passed by reference 
+class test {
+    int a;
+    test(int i){
+        a = i;
+    }
+    test incrbyten(){
+        test temp =  new test (a+10);
+        return temp ;
+    }
+ }
+public class Main
+{
+	public static void main(String[] args) {
+	test ob1= new test(2);
+	test ob2;
+	
+	ob2 = ob1.incrbyten();
+	
+	System.out.println("ob1.a :"+ ob1.a);
+		System.out.println("ob2.a :"+ ob2.a);
+
+   ob2 = ob2.incrbyten();
+   System.out.println("ob2.a after second increase :"+ ob2.a);
+	
+		}
+	}
