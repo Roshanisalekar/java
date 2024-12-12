82fc0067524d83722bbcf2186654fd6d11fd1458
roshani <roshanisalekar3046@gmail.com>
2024-12-12 21:42:36 +0530

    Add files via upload

---
+++ super variable access .java
@@ -0,0 +1,19 @@
+class A{
+    int a = 10 ;
+    
+}
+class B extends A {
+    int a = 20;
+    void show(){
+        System.out.println(a);
+         System.out.println(super.a);
+    }
+    
+}
+public class Main
+{
+	public static void main(String[] args) {
+		B r = new B();
+		r.show();
+	}
+}
