af6cb680dedc94283133e50e9a36faf6569bdb23
roshani <roshanisalekar3046@gmail.com>
2024-12-20 22:20:20 +0530

    Add files via upload

---
+++ increment and decrement operator .java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main 
+{
+	public static void main(String[] args) {
+	    int a = 10;
+   System.out.println("increment and decrement operator ");
+   System.out.println("prepost increment :");
+   System.out.println(a);
+   System.out.println(a++);
+    System.out.println(a);
+   System.out.println(++a);
+   System.out.println();
+    System.out.println("prepost decrement :");
+   System.out.println(a);
+   System.out.println(a--);
+    System.out.println(a);
+   System.out.println(--a);
+	}
+}
