259cbd8bc16e16bc22ab20133bf94d882440b261
roshani <roshanisalekar3046@gmail.com>
2024-12-29 12:22:52 +0530

    Add files via upload

---
+++ nested if else.java
@@ -0,0 +1,23 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+    int a =1, b=20, c=0;
+    if(a>b){
+        if(a>c){
+            System.out.println(a);
+        }
+        else{
+            System.out.println(c);
+        }
+    }
+    else{
+        if(b>c){
+            System.out.println(b);
+        }
+        else{
+            System.out.println(c);
+        }
+    }
+	}
+}
