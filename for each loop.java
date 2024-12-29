d1120f228f45bac63e8476bfbb5411b356ce5a36
roshani <roshanisalekar3046@gmail.com>
2024-12-29 19:15:17 +0530

    Add files via upload

---
+++ for each loop.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+    int a[] ={10,20,30,40,50,60,70,80};
+    for(int b : a){
+        System.out.print(b+" ");
+    }
+	 System.out.print("\nThanks you");
+	}
+}
