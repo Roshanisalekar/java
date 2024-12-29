9f5449d7a579ebe96a037fff8653d3d5fb74a9f4
roshani <roshanisalekar3046@gmail.com>
2024-12-29 18:15:42 +0530

    Add files via upload

---
+++ while loop.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	 int n;
+	 System.out.println("Enter any value :");
+	 Scanner r = new Scanner(System.in);
+	 n=r.nextInt();
+
+	 while(n<=10){
+	     System.out.println("learn java");
+	     n++;
+	 }
+	 System.out.println("Thank you");
+	}
+}
