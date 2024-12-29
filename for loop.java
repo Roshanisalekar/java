8e3ea3eef37b390c7153937548498cc4beeec346
roshani <roshanisalekar3046@gmail.com>
2024-12-29 18:59:39 +0530

    Add files via upload

---
+++ for loop.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+
+     for (int n=1 ;n<=10 ;n++ ) {  //natural number 
+        System.out.println(n +" ");
+     }
+	 System.out.println("Thank you");
+	}
+}
