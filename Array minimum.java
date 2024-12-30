7518d236d9e7f3c2bc95fff8d46be86c944f19c2
roshani <roshanisalekar3046@gmail.com>
2024-12-30 23:03:19 +0530

    Add files via upload

---
+++ Array minimum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+    int a[]=new int [5];
+    int max;
+    Scanner r= new Scanner(System.in);
+    System.out.println("Enter arrays element :");
+    for(int i=0;i<5;i++){
+        a[i]=r.nextInt();
+    }
+     max=a[0];
+    for(int i=1;i<5;i++)
+    {
+        if(a[i]<max)
+        {
+            max=a[i];
+        }
+    }
+    System.out.println("minimum value :"+max);
+	}
+}
