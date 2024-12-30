0750ae9d620742260c042873a77dbea6b5906e98
roshani <roshanisalekar3046@gmail.com>
2024-12-30 10:35:42 +0530

    Add files via upload

---
+++ Array print.java
@@ -0,0 +1,22 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+		int a []=new int[3];
+	
+		a[0]=10;
+		a[1]=20;
+		a[2]=30;
+		
+		// for loop print array
+		
+			/*	for(int i=0;i<3;i++){
+				    System.out.println(a[i]);
+				    	}
+				    	*/
+			//for-each loop print array
+			for(int b : a){
+			 System.out.print(b+" ");
+				    }
+				}
+	}
