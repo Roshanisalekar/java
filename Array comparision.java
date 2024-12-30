8f302416c3057010c8df119286724eb0c3b22d9b
roshani <roshanisalekar3046@gmail.com>
2024-12-30 22:37:20 +0530

    Add files via upload

---
+++ Array comparision.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+public class Main
+{
+	public static void main(String[] args) {
+	int a[]={10,20,30,40,50,60};
+	int b[]={10,20,30,40,50,60};
+	if(Arrays.equals(a,b)){
+	    System.out.println("both are equals");
+	}
+	else{
+	    System.out.println("both are not equals"); 
+	}
+	}
+}
