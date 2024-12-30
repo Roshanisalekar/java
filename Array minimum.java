8f921e3e480ebf3befb7f8e1a0a5fb38eae6a06b
roshani <roshanisalekar3046@gmail.com>
2024-12-30 09:37:50 -0800

    Update Array minimum.java

--- Array minimum.java
+++ Array minimum.java
@@ -3,20 +3,20 @@ public class Main
 {
 	public static void main(String[] args) {
     int a[]=new int [5];
-    int max;
+    int min;
     Scanner r= new Scanner(System.in);
     System.out.println("Enter arrays element :");
     for(int i=0;i<5;i++){
         a[i]=r.nextInt();
     }
-     max=a[0];
+     min=a[0];
     for(int i=1;i<5;i++)
     {
-        if(a[i]<max)
+        if(a[i]<min)
         {
-            max=a[i];
+            min=a[i];
         }
     }
-    System.out.println("minimum value :"+max);
+    System.out.println("minimum value :"+min);
 	}
 }
